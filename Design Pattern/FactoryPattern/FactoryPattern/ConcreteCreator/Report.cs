2dcae806b29af61f732fd52c2415991e9a3d4f4e
Sumit Anand <sumitrockey@gmail.com>
2018-08-13 13:53:36 +0530

    #30DaysCodeChallenge
    Design Pattern - Factory Pattern

---
+++ Design Pattern/FactoryPattern/FactoryPattern/ConcreteCreator/Report.cs
@@ -0,0 +1,13 @@
+﻿using FactoryPattern.ConcreteProduct;
+
+namespace FactoryPattern.ConcreteCreator
+{
+    class Report : Document
+    {
+        public override void CreatePages()
+        {
+            Pages.Add(new Bibliography());
+            Pages.Add(new Summary());
+        }
+    }
+}
---
+++ Design Pattern/FactoryPattern/FactoryPattern/ConcreteCreator/Resume.cs
@@ -0,0 +1,13 @@
+﻿using FactoryPattern.ConcreteProduct;
+
+namespace FactoryPattern.ConcreteCreator
+{
+    class Resume : Document
+    {
+        public override void CreatePages()
+        {
+            Pages.Add(new Experience());
+            Pages.Add(new Skills());
+        }
+    }
+}
